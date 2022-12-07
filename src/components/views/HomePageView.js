468e0e6665d69b84c1e35cc9f86691c0bf518164
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-12-06 22:38:53 -0500

    Link to students

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -8,6 +8,7 @@ import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
 /*==================================================
 HomePageView.js
 
@@ -32,46 +33,50 @@ const HomePageView = () => {
         style={{ minHeight: '100vh', maxWidth: '100%' }}
       >
         <Grid item xs={6} align="center" spacing={30}>
-          <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="cunyStudents"
-                height="800"
-                image="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/global/diversity.jpg"
-                title="cunyStudents"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  All Students
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Click to see all students
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          <Link to={'/students'}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="cunyStudents"
+                  height="800"
+                  image="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/global/diversity.jpg"
+                  title="cunyStudents"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    All Students
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    Click to see all students
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>
         </Grid>
         <Grid item xs={6} align="center">
-          <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="cunyStudents"
-                height="800"
-                image="https://macaulay.cuny.edu/wp-content/uploads/2016/07/qc10_bg_000056-1920x1080.jpg"
-                title="cunyStudents"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  All Campuses
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Click to see all Campuses
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          <Link to={'/campuses'}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="cunyStudents"
+                  height="800"
+                  image="https://macaulay.cuny.edu/wp-content/uploads/2016/07/qc10_bg_000056-1920x1080.jpg"
+                  title="cunyStudents"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    All Campuses
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    Click to see all Campuses
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>
         </Grid>
       </Grid>
     </>
