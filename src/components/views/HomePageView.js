e4bc08f09148706f8a895db7f369fbc8f0356dad
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-12-06 20:44:11 -0500

    install MUI into the main frame and create homepage

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,16 +1,71 @@
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Unstable_Grid2'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
 /*==================================================
 HomePageView.js
 
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+
 const HomePageView = () => {
   // Render Home page view
   return (
-    <div >
-      <h1>Home Page</h1>
-    </div>
-  );    
+    <>
+      <Grid
+        container
+        justify="center"
+        style={{ minHeight: '100vh', maxWidth: '100%' }}
+      >
+        <Grid item xs={6} align="center">
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                All Students
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6} align="center">
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                All Campuses
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
-export default HomePageView;
+export default HomePageView
