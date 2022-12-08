950545a747972df53eb09b68891da557441f53cc
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-12-07 22:54:38 -0500

    add redux componenet and delete campuses

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,24 +1,27 @@
+import Button from '@mui/material/Button'
 /*==================================================
 AllCampusesView.js
 
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display all campuses.
 ================================================== */
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const AllCampusesView = (props) => {
   // If there is no campus, display a message.
+  const { allCampuses, deleteCampus } = props
+
   if (!props.allCampuses.length) {
-    return <div>There are no campuses.</div>;
+    return <div>There are no campuses.</div>
   }
 
-  // If there is at least one campus, render All Campuses view 
+  // If there is at least one campus, render All Campuses view
   return (
     <div>
       <h1>All Campuses</h1>
 
-      {props.allCampuses.map((campus) => (
+      {allCampuses.map((campus) => (
         <div key={campus.id}>
           <Link to={`/campus/${campus.id}`}>
             <h2>{campus.name}</h2>
@@ -26,20 +29,24 @@ const AllCampusesView = (props) => {
           <h4>campus id: {campus.id}</h4>
           <p>{campus.address}</p>
           <p>{campus.description}</p>
-          <hr/>
+          <Button variant="outlined" onClick={() => deleteCampus(campus.id)}>
+            Remove
+          </Button>
+          <hr />
         </div>
       ))}
-      <br/>
+      <br />
       <Link to={`/`}>
         <button>Add New Campus</button>
       </Link>
-      <br/><br/>
+      <br />
+      <br />
     </div>
-  );
-};
+  )
+}
 
 AllCampusesView.propTypes = {
   allCampuses: PropTypes.array.isRequired,
-};
+}
 
-export default AllCampusesView;
+export default AllCampusesView
